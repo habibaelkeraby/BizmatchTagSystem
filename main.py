07a1d14e724cdab76f6f0a9b9393faf0e9b4fd63
habibaelkeraby <133785264+habibaelkeraby@users.noreply.github.com>
2023-10-23 11:38:12 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ Generate tag groups to match a company to a wider network.
 """)
 
 with st.expander("Follow the steps below to generate tag groupings:"):
-  st.write("""1. Upload a .csv file of tags (industries) OR paste a list of tags, separated by a new line, into the text box.
+  st.write(
+      """1. Upload a .csv file of tags (industries) OR paste a list of tags, separated by a new line, into the text box.
             \n2. Click the button **Group Tags** to generate tag groupings.
             \n3. View the .csv file of tag groupings.
             \n4. Download the .csv file of tag groupings by clicking the **Download** button.
@@ -20,7 +21,7 @@ with st.expander("Follow the steps below to generate tag groupings:"):
             """)
 
 # File Uploader
-uploaded_file = st.file_uploader("Choose a CSV file", type = 'csv')
+uploaded_file = st.file_uploader("Choose a CSV file", type='csv')
 if uploaded_file is not None:
   # Can be used wherever a "file-like" object is accepted:
   dataframe = pd.read_csv(uploaded_file)
@@ -31,10 +32,10 @@ if uploaded_file is not None:
 st.subheader('st.text_area')
 input_tags = st.text_area('Paste tags here')
 tags = input_tags.split("\n")
+# output
 st.write(input_tags)
 #tags_df = pd.DataFrame(input_tags)
 #st.dataframe(tags_df)
-
 
 # Enable webview in replit
 os.system("streamlit run main.py --server.enableCORS false")
