3b3a1db37dc10a7f2a90dcec39f2954b9a30fd5b
habibaelkeraby <133785264+habibaelkeraby@users.noreply.github.com>
2023-10-26 13:15:45 +0000

    Added tag groupings using gensim-word2vec

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from gensim.models import Word2Vec
 import warnings
 
 warnings.filterwarnings('ignore')
@@ -37,7 +38,7 @@ with st.expander("Guidelines on formatting of csv file:"):
             \n3. The contents of the file must be in English.
             """)
 # Column structure to visualize data
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 # Processing uploaded file
 if uploaded_file is not None:
@@ -76,8 +77,48 @@ if uploaded_file is not None:
   tag_groups_df = pd.DataFrame.from_dict(tag_groups, orient='index')
   # Visualize generated tag groups
   with col2:
-    st.write("Generated Tag Groups")
+    st.write("Generated Tag Groups (TF-IDF vectorizer)")
     st.write(tag_groups_df)
+
+  ######################################################################
+  # GENSIM Application
+  # Preprocess the tags: split into words and make them lowercase
+  tag_tokens = [tag.lower().split() for tag in tags]
+
+  # Create and train a Word2Vec model
+  model = Word2Vec(sentences=tag_tokens, vector_size=100, window=5, min_count=1, sg=0)
+
+  # Define a function to calculate the vector representation of a tag
+  def get_tag_vector(tag):
+      tokens = tag.lower().split()
+      vector = np.zeros(model.vector_size)
+      for token in tokens:
+          if token in model.wv:
+              vector += model.wv[token]
+      return vector
+
+  # Calculate cosine similarity between all tag pairs
+  cosine_sim_gensim = cosine_similarity([get_tag_vector(tag) for tag in tags])
+
+  # Define a threshold for considering tags similar
+  threshold_gensim = 0.5
+
+  # Create a dictionary to store groups of similar tags
+  tag_groups_gensim = {}
+
+  # Iterate through the tags and find similar tags
+  for i, tag in enumerate(tags):
+      similar_tags_gensim = [tags[j] for j in range(len(tags)) if cosine_sim_gensim[i][j] > threshold_gensim]
+      tag_groups_gensim[tag] = similar_tags_gensim
+
+  tag_groups_gensim_df = pd.DataFrame.from_dict(tag_groups_gensim, orient='index')
+  # Visualize generated tag groups
+  with col3:
+    st.write("Generated Tag Groups (Gensim-Word2Vec)")
+    st.write(tag_groups_gensim_df)
+  # Print the tag groups
+  #for tag, similar_tags_gensim in tag_groups_gensim.items():
+      #print(tag, ":", similar_tags_gensim)
 
   ######################################################################
   # Download button to download the csv file
@@ -86,18 +127,32 @@ if uploaded_file is not None:
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
 
-  csv = convert_df(tag_groups_df)
+  csv_tfidf = convert_df(tag_groups_df)
+  csv_gensim = convert_df(tag_groups_gensim_df)
 
-  if st.download_button(
-      label="Download",
-      type="primary",
-      data=csv,
-      file_name='tag_groups.csv',
-      mime='text/csv',
-  ):
-    st.write('File Downloaded')
-  else:
-    st.write('')
+  with col2:
+    if st.download_button(
+        label="Download",
+        type="primary",
+        data=csv_tfidf,
+        file_name='tag_groups_tfidf.csv',
+        mime='text/csv',
+    ):
+      st.write('File Downloaded')
+    else:
+      st.write('')
+
+    with col3:
+      if st.download_button(
+          label="Download",
+          type="primary",
+          data=csv_gensim,
+          file_name='tag_groups_gensim.csv',
+          mime='text/csv',
+      ):
+        st.write('File Downloaded')
+      else:
+        st.write('')
 
     # Print the tag groups
     #for tag, similar_tags in tag_groups.items():
@@ -110,12 +165,25 @@ if uploaded_file is not None:
     options = st.multiselect(
         'Select tag(s) to check the tags they will be matched to:', tags)
 
+    # Columns for printing
+    col_tfidf, col_gensim= st.columns(2)
+
     # Every form must have a submit button.
     submitted = st.form_submit_button("Submit")
     if submitted:
+      with col_tfidf:
+        st.write("TF-IDF vectorizer")
+      with col_gensim:
+        st.write("Gensim-Word2Vec")
       for tag, similar_tags in tag_groups.items():
         for option in options:
           if option == tag:
-            st.write(tag, ":", similar_tags)
+            with col_tfidf:
+              st.write(tag, ":", similar_tags)
+      for tag, similar_tags in tag_groups_gensim.items():
+        for option in options:
+          if option == tag:
+            with col_gensim:
+              st.write(tag, ":", similar_tags)
     else:
       st.write('')
